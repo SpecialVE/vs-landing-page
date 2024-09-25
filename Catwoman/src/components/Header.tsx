e2ae00dca386fa6619d78b866cb9feb2d6b956bf
Victory Special Emmanuel <victoryspecialemmanuel@gmail.com>
2024-09-25 22:32:56 +0000

    change

---
+++ Catwoman/src/components/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="menu-icon">
+        {/* You can use a hamburger icon (e.g., from font-awesome or any icon library) */}
+        <span>☰</span>
+      </div>
+      <h1 className="project-title">frontend-project</h1>
+    </header>
+  );
+};
+
+export default Header;
