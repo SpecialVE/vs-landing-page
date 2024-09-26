de5fbfdc14eb56dbd9c3c013f72b4086d0a6dd9e
Victory Special Emmanuel <victoryspecialemmanuel@gmail.com>
2024-09-26 16:53:17 +0000

    changes

--- Catwoman/src/App.tsx
+++ Catwoman/src/App.tsx
@@ -2,27 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import Profile from './components/Profile';
 import Header from './components/Header';
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      
+function  App() {
+  return  (
+    <div  className='App'>
+      <Header />
       <Profile />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
-
-export default App
+export default App;
