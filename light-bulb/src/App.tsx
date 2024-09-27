56a489491fb6c6043b203bc6aafd44d50980b29b
rehkmansa <godspowernathaniel25@gmail.com>
2024-09-27 11:57:36 +0100

    LIGHTBULB: Create lightbulub task

--- light-bulb/src/App.tsx
+++ light-bulb/src/App.tsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/**
+ * Task, When I click on the button it should make the page bright(use a white bg) and text should say darken
+ * Clicking on darken too should do the opposite, black bg, and text should change
+ *
+ * Don't forget to update text colors as you cycle between light and dark bg
+ */
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <div className="bg-black h-screen grid place-items-center text-white">
+      <button type="button">Brighten</button>
+    </div>
+  );
+};
