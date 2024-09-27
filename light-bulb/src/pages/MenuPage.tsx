7371c5a2dfed337a30d0e689ad2e22ed5aa27b62
Victory Special Emmanuel <victoryspecialemmanuel@gmail.com>
2024-09-27 20:06:45 +0000

    LIGHTBULB: Create components

---
+++ light-bulb/src/pages/MenuPage.tsx
@@ -0,0 +1,15 @@
+const MenuPage = () => {
+    return (
+      <div className="menu-page">
+        <h1>Rehk Mansa</h1>
+        <nav>
+          <ul>
+            <li>00 Projects</li>
+            <li>01 Resume</li>
+          </ul>
+        </nav>
+      </div>
+    );
+  };
+  
+  export default MenuPage;
