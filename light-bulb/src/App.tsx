ca8f861495b51d502345b168fd1e534d01af44fd
Victory Special Emmanuel <victoryspecialemmanuel@gmail.com>
2024-10-01 19:25:30 +0000

    LIGHTBULB: Update App.tsx fie

--- light-bulb/src/App.tsx
+++ light-bulb/src/App.tsx
@@ -3,17 +3,34 @@
  * Clicking on darken too should do the opposite, black bg, and text should change
  *
  * Don't forget to update text colors as you cycle between light and dark bg
- */
-import HomePage from "./pages/HomePage";
-import MenuPage from "./pages/MenuPage";
+ **/
 
+import { useState } from "react";
+import './styles.css';
 
-export const App = () => {
+export const App: React.FC = () => {
+  const [isBright, setIsBright] = useState<boolean>(false);
+
+  // Properly toggling the state
+  const toggleBrightness = () => {
+    setIsBright((prev) => !prev);
+  };
+
   return (
-    <div className="bg-black h-screen grid place-items-center text-white">
-      <button type="button">Brighten</button>
-      <HomePage />
-      <MenuPage />
+    <div
+      className={`h-screen flex items-center justify-center transition-all duration-300 ${
+        isBright ? 'bg-white text-black' : 'bg-black text-white'
+      }`}
+    >
+      <button
+        type="button"
+        onClick={toggleBrightness}
+        className="px-4 py-2 border rounded focus:outline-none focus:ring"
+      >
+        {isBright ? 'Darken' : 'Brighten'}
+      </button>
     </div>
   );
 };
+
+export default App;
