59aab95bd38b1d64c4495a60fe9f83c9a189f996
Special Emmanuel <122409630+Iamsigur@users.noreply.github.com>
2024-10-09 22:10:27 +0100

    SHE-HULK: Component Update

--- she-hulk/src/App.tsx
+++ she-hulk/src/App.tsx
@@ -14,9 +14,7 @@ function App() {
 
   return (
     <main className="py-10 h-screen space-y-5">
-      <h1 className="font-bold text-3xl text-center">
-        <My></My> Todos
-      </h1>
+      <h1 className="font-bold text-3xl text-center">My Todos</h1>
       <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6 ">
         <AddTodoForm />
         <div className="space-y-2">
--- she-hulk/src/components/AddTodoForm.tsx
+++ she-hulk/src/components/AddTodoForm.tsx
@@ -1,11 +1,34 @@
-export default function AddTodoForm() {
+import { useState } from "react";
+
+interface AddTodoFormProps {
+    onSubmit: (title: string) => void;
+}
+
+export default function AddTodoForm({onSubmit}: AddTodoFormProps) {
+  const [input, setInput] = useState("");
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (!input.trim()) return;
+
+    onSubmit(input)
+  }
+
   return (
-    <form className="flex">
+    <form className="flex" onSubmit={handleSubmit}>
       <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
         placeholder="Enter new todo item"
         className="rounded-s-md grow border border-gray-400 p-2"
       />
-      <button type="">Add</button>
+      <button
+        type="submit"
+        className="w-16 rounded-e-md bg-slate-900 text-white hover:bg-slate-800"
+      >
+        Add
+      </button>
     </form>
   );
 }
