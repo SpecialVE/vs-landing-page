0dbcbb567947a7aef1ac7de6733229485bb9cf99
Special Emmanuel <122409630+Iamsigur@users.noreply.github.com>
2024-10-11 00:20:27 +0100

    SHE-HULK: Update component

---
+++ she-hulk/src/components/TodoList.tsx
@@ -0,0 +1,18 @@
+import { Todo } from "./types/todo";
+import TodoItem from "./TodoItem";
+
+interface TodoListProps {
+  todos: Todo[];
+  onCompletedChange: (id: number, completed: boolean) => void;
+  onDelete: (id: number) => void;
+}
+
+export default function TodoList({ todos, onCompletedChange, onDelete }: TodoListProps) {
+  return (
+    <div className="space-y-2">
+      {todos.map((todo) => (
+        <TodoItem key={todo.id} todo={todo} onCompletedChange={onCompletedChange} />
+      ))}
+    </div>
+  );
+}
